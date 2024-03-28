28fd8a17f1d654e88efa582f465a13ce392bd6ef
mrltiana <mymeltiana@gmail.com>
2024-03-28 12:47:37 +0700

    Contoh1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,16 @@
+<?php
+
+class Contoh1 extends CI_Controller 
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "Nama Saya Meltiana
+              Saya tinggal diPontianak
+              Olah raga yang saya sukai adalah
+              Bulu Tangkis";
+
+    }
+
+
+}
